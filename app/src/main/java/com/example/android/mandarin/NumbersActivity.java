2893b06522ed6012d24bc25abc1c9a4b1c901ccf
Rob ABCBA <rob_abcba@yahoo.com>
2018-11-22 12:04:30 -0800

    NumbersActivity.java: Replaced the standard Android ArrayAdapter with my custom WordAdapter.  Commit from BeMaMi 2018-11-22.

--- app/src/main/java/com/example/android/mandarin/NumbersActivity.java
+++ app/src/main/java/com/example/android/mandarin/NumbersActivity.java
@@ -15,10 +15,13 @@
  */
 package com.example.android.mandarin;
 
+import android.provider.UserDictionary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,50 +34,52 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Create an array of words
-        ArrayList<String> words = new ArrayList<String>();
+        // Change the ArrayList of Strings called "words" into an ArrayList of Word objects
+        ArrayList<Word> words = new ArrayList<Word>();
+        // RL ArrayList<String> list = new ArrayList<String>();
 
-        words.add("yi");
-        words.add("er");
-        words.add("san");
-        words.add("si");
-        words.add("wu");
-        words.add("liu");
-        words.add("qi");
-        words.add("ba");
-        words.add("jiu");
-        words.add("shi");
-        words.add("shi yi");
-        words.add("shi er");
-        words.add("shi san");
-        words.add("shi si");
-        words.add("shi wu");
-        words.add("shi liu");
-        words.add("shi qi");
-        words.add("shi ba");
-        words.add("shi jiu");
-        words.add("er shi");
-        words.add("er shi yi");
-
-
+        // words.add("yi");
+        words.add(new Word("one","yi"));
+        words.add(new Word("two","er"));
+        words.add(new Word("three","san"));
+        words.add(new Word("four","si"));
+        words.add(new Word("five","wu"));
+        words.add(new Word("six","liu"));
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
         // adapter knows how to create layouts for each item in the list, using the
         // simple_list_item_1.xml layout resource defined in the Android framework.
         // This list item layout contains a single {@link TextView}, which the adapter will set to
         // display a single word.
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        // RL ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        // Same as above except data source is a list of Word objects.
+        // Replace the Android team's generic simple_list_item_1.xml layout resource with our own
+        // custom list_item layout resource.  Our list item layout contains two {@link TextView}, which
+        // our adapter will set to a Mandarin word on top of the corresponding English word.
+        // RL ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words);
+
+        // Same as above except no need to use Android's ArrayAdapter with our list_item resource choice.
+        // Instead create our own Adapter and call it WordAdapter.  It will always use our list_item layout
+        // so no need to have that as a parameter anymore.
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // activity_numbers.xml layout file.
+        // Experiment with different types of views (GridView, etc.). Then change back to ListView.
+        // RL GridView listView = (GridView) findViewById(R.id.list);
         ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         // {@link ListView} will display list items for each word in the list of words.
         // Do this by calling the setAdapter method on the {@link ListView} object and pass in
         // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
+        // RL listView.setAdapter(itemsAdapter);
+        // Same as above except pass in the {@link WordAdapter} with the variable name adapter.
+        listView.setAdapter(adapter);
 
     }
 }
