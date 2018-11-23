e390fe7149649135e0e6e192ce09de1491ce543a
Rob ABCBA <rob_abcba@yahoo.com>
2018-11-23 07:41:46 -0800

    Word.java: Check whether have corresponding Image to go along with the English and Pinyin strings.

--- app/src/main/java/com/example/android/mandarin/Word.java
+++ app/src/main/java/com/example/android/mandarin/Word.java
@@ -13,9 +13,22 @@ public class Word {
     /** Mandarin translation for the word */
     private String mPinyinTranslation;
 
+    /** Image resource ID */
+    private int mImageResourceId;
+
+    /** Set at No Image state by default */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
     public Word(String englishTranslation, String pinyinTranslation) {
         mEnglishTranslation = englishTranslation;
         mPinyinTranslation = pinyinTranslation;
+    }
+
+    public Word(String englishTranslation, String pinyinTranslation, int imageResourceId) {
+        mEnglishTranslation = englishTranslation;
+        mPinyinTranslation = pinyinTranslation;
+        mImageResourceId = imageResourceId;
     }
 
 
@@ -23,7 +36,6 @@ public class Word {
      * Get the Engilsh translation of the word.
      * @return
      */
-
     public String getEnglishTranslation() {
         return mEnglishTranslation;
     }
@@ -32,9 +44,28 @@ public class Word {
      * Get the Pinyin translation of the word.
      * @return
      */
-
     public String getPinyinTranslation() {
         return mPinyinTranslation;
     }
+
+    /**
+     * Get Image resource ID.
+     * @return
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word/phrase.
+     * If mImageResourceId got changed to imageResourceId up in the public Word constructor
+     * section of this routine, that means that Word entry DOES have a valid image associated with it.
+     * If mImageResourceId is still stuck at its initial value of NO_IMAGE_PROVIDED, then we know
+     * that Word entry does NOT have a valid image associated with it.
+     */
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
 
 }
