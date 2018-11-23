8966eeddc6d652daab61918900dc96e1ae53737b
Rob ABCBA <rob_abcba@yahoo.com>
2018-11-23 06:37:26 -0800

    WordAdapter.java: Uncomment 2 lines to add appropriate image to left of current vocabulary word entries of current Word object.

--- app/src/main/java/com/example/android/mandarin/WordAdapter.java
+++ app/src/main/java/com/example/android/mandarin/WordAdapter.java
@@ -70,10 +70,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         numberTextView.setText(currentWord.getEnglishTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        // RL ... ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.image);
         // Get the image resource ID from the current Word object and
         // set the image to iconView
-        // RL ... iconView.setImageResource(currentWord.getImageResourceId());
+        iconView.setImageResource(currentWord.getImageResourceId());
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
