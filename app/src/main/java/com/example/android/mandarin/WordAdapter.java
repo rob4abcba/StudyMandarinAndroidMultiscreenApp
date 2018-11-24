32573be4ab62a9bda436e3e530e407aa77522904
Rob <rob_abcba@yahoo.com>
2018-11-24 10:59:57 -0800

    Modify WordAdapter.java to grab the Id of the Text portion of the list_item and assign it different background colors based on input arguments passed to the WordAdapter within each of the Activity.java files (ColorsActivity.java, FamilyActivity.java, KaraokeActivity.java, NumbersActivity.java, and PhrasesActivity.java).  Total of 7 files changed (6 .java files + 1 .xml file)

--- app/src/main/java/com/example/android/mandarin/WordAdapter.java
+++ app/src/main/java/com/example/android/mandarin/WordAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.mandarin;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,9 @@ import java.util.ArrayList;
  * */
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    /** Resource ID for the background color for this list of words */
+    private int mColorResourceId;
+
 
 
     private static final String LOG_TAG = WordAdapter.class.getSimpleName();
@@ -28,12 +33,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
      * @param context        The current context. Used to inflate the layout file.
      * @param words A List of Word objects to display in a list
      */
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     /**
@@ -90,6 +96,12 @@ public class WordAdapter extends ArrayAdapter<Word> {
             // Otherwise hide the ImageView
             imageView.setVisibility(View.GONE);
         }
+
+        // Set theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
